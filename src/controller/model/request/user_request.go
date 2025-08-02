52aae1cc9bd16ea054ddb65460cd63bc05bd7dad
Bernardo-git-dev <bernardotati0305@gmail.com>
2025-08-02 20:38:52 +0100

    feat: create model request/response

--- src/controller/model/request/user_request.go
+++ src/controller/model/request/user_request.go
@@ -1,1 +1,9 @@
 package request
+
+type UserRequest struct {
+	ID       string `json:"id"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6,max=20"`
+	Name     string `json:"name" binding:"required,min=3,max=50"`
+	Age      string `json:"name" binding:"required,min=3,max=50"`
+}
