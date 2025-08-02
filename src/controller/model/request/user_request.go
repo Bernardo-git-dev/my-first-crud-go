17b6b89777791ee96d38574f8eb8c36e5bcd823b
Bernardo-git-dev <bernardotati0305@gmail.com>
2025-08-02 20:40:41 +0100

    feat: add tags

--- src/controller/model/request/user_request.go
+++ src/controller/model/request/user_request.go
@@ -1,7 +1,7 @@
 package request
 
 type UserRequest struct {
-	ID       string `json:"id"`
+	ID       string `json:"id,omitempty"`
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required,min=6,max=20"`
 	Name     string `json:"name" binding:"required,min=3,max=50"`
--- src/controller/model/response/user_response.go
+++ src/controller/model/response/user_response.go
@@ -1,7 +1,7 @@
 package response
 
 type UserResponse struct {
-	ID    string `json:"id"`
+	ID    string `json:"id",omitempty`
 	Email string `json:"email" binding:"required,email"`
 	Name  string `json:"name" binding:"required,min=3,max=50"`
 	Age   int8   `json:"age" binding:"required,min=18,max=100"`
