b7caf971eb82aa07c760e60479598a44b5b9139e
Bernardo-git-dev <bernardotati0305@gmail.com>
2025-08-03 18:08:33 +0100

    feat: add logger

--- go.mod
+++ go.mod
@@ -4,7 +4,11 @@ go 1.24.5
 
 require github.com/joho/godotenv v1.5.1
 
-require github.com/go-playground/validator v9.31.0+incompatible // indirect
+require (
+	github.com/go-playground/validator v9.31.0+incompatible // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+)
 
 require (
 	github.com/bytedance/sonic v1.14.0 // indirect
--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,22 +1,40 @@
 package controller
 
 import (
-	"fmt"
+	"net/http"
 
-	"github.com/gin-gonic/gin"
-
+	"github.com/Bernardo-git-dev/my-first-crud-go/src/configuration/logger"
 	"github.com/Bernardo-git-dev/my-first-crud-go/src/configuration/validation"
 	"github.com/Bernardo-git-dev/my-first-crud-go/src/controller/model/request"
+	"github.com/Bernardo-git-dev/my-first-crud-go/src/controller/model/response"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Init CreateUser controller",
+		zap.String("jurney", "CreateUser"),
+	)
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Error trying to validate user info", err,
+			zap.String("jurney", "CreateUser"),
+		)
 		restErr := validation.ValidateUserError(err)
-
 		c.JSON(restErr.Code, restErr)
 		return
 	}
-	fmt.Println(userRequest)
+
+	response := response.UserResponse{
+		ID:    "test",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+	logger.Info("User created successfully",
+		zap.String("jurney", "CreateUser"),
+	)
+
+	c.JSON(http.StatusOK, response)
 }
